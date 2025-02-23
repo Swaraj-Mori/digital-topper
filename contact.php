9d4af4b4d3a16d95ed4b5397c0631f96862f56fd
Swaraj <160823446+Swaraj-Mori@users.noreply.github.com>
2025-02-24 00:31:06 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = "your-email@example.com";
+    $to = "digitaltopper.in@gmail.com";
     $subject = "New Contact Form Submission";
     $headers = "From: " . $email;
 
